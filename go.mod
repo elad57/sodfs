e1add8bf65c17cd6a112f1e085691825284531b0
elad57 <eladharel0011@gmail.com>
2025-09-27 16:59:07 +0300

    fix: cruptofunctions names

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/elad57/sodfs
 
 go 1.22.1
 
-require github.com/elad57/sodfs/cryptofunctions v0.0.0-20250927135431-173a2d8f8b7c
+require github.com/elad57/sodfs/cryptofunctions v0.0.0-20250927135611-d6d733a1f2d5
--- sodfs.go
+++ sodfs.go
@@ -1,81 +1,14 @@
 package sodfs
 
-//import file system
 import (
-	// "crypto/aes"
-	// "errors"
 	"os"
 	"github.com/elad57/sodfs/cryptofunctions"
-// 	"crypto/cipher"
-// 	"crypto/rand"
-// 	"crypto/sha256"
-// 	"io"
 )
 
 type SODFS struct {
 	EncryptionKey string
 }
 
-// func encrypt(data []byte, key string) ([]byte, error) {
-// 	keyHash := sha256.Sum256([]byte(key))
-	
-// 	// Create AES cipher block
-// 	block, err := aes.NewCipher(keyHash[:])
-// 	if err != nil {
-// 		return nil, err
-// 	}
-	
-// 	// Create GCM mode
-// 	gcm, err := cipher.NewGCM(block)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-	
-// 	// Create nonce (number used once)
-// 	nonce := make([]byte, gcm.NonceSize())
-// 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-// 		return nil, err
-// 	}
-	
-// 	// Encrypt and authenticate the data
-// 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-	
-// 	return ciphertext, nil
-// }
-
-// func decrypt(data []byte, key string) ([]byte, error) {
-// 	// Create a SHA256 hash of the key to ensure it's 32 bytes (AES-256)
-// 	keyHash := sha256.Sum256([]byte(key))
-	
-// 	// Create AES cipher block
-// 	block, err := aes.NewCipher(keyHash[:])
-// 	if err != nil {
-// 		return nil, err
-// 	}
-	
-// 	// Create GCM mode
-// 	gcm, err := cipher.NewGCM(block)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-	
-// 	// Check if data is long enough to contain nonce
-// 	nonceSize := gcm.NonceSize()
-// 	if len(data) < nonceSize {
-// 		return nil, errors.New("ciphertext too short")
-// 	}
-	
-// 	// Extract nonce and ciphertext
-// 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-	
-// 	// Decrypt and verify the data
-// 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-	
-// 	return plaintext, nil
-// }
 
 func (sodfs *SODFS) WriteFile(filePath string, data []byte) error {
 	file, err := os.Create(filePath)
@@ -84,7 +17,7 @@ func (sodfs *SODFS) WriteFile(filePath string, data []byte) error {
 	}
 	defer file.Close()
 
-	encryptedData, err := cryptofunctions.encrypt(data, sodfs.EncryptionKey)
+	encryptedData, err := cryptofunctions.Encrypt(data, sodfs.EncryptionKey)
 	if err != nil {
 		return err
 	}
@@ -112,7 +45,7 @@ func (sodfs *SODFS) ReadFile(filePath string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	decryptedData, err := cryptofunctions.decrypt(data, sodfs.EncryptionKey)
+	decryptedData, err := cryptofunctions.Decrypt(data, sodfs.EncryptionKey)
 	if err != nil {
 		return nil, err
 	}
